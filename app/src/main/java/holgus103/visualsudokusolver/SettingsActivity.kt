4beca0ab7498a8ce01dcec06d5c3bb12f1302810
holgus103 <kubasuchan@hotmail.com>
2018-04-04 02:58:21 +0200

    IU draft + Use case

---
+++ app/src/main/java/holgus103/visualsudokusolver/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package holgus103.visualsudokusolver
+
+import android.os.Bundle
+import android.preference.PreferenceActivity
+
+class SettingsActivity : PreferenceActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
